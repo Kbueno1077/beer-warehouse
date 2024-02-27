5d5ae3514567ccfe79887230f2c2c714e2769b83
Kbueno1077 <kbueno1077@gmail.com>
2024-02-26 20:12:12 -0500

    Fix Color select option

--- modules/BeerDetails/BeerDetails.tsx
+++ modules/BeerDetails/BeerDetails.tsx
@@ -7,6 +7,7 @@ import {useBeerStore} from "@/store/zustand";
 import NextImage from "next/image";
 import {Image} from "@nextui-org/react";
 import FlagAvatar from "@/components/FlagAvatar/FlagAvatar";
+import {useTranslations} from "next-intl";
 
 interface BeerDetailsProps {
     beer: BeerType
@@ -15,39 +16,68 @@ interface BeerDetailsProps {
 
 function BeerDetails({beer}: BeerDetailsProps) {
     const {theme} = useBeerStore();
+    const t = useTranslations('beerDetails')
 
     console.log(beer)
 
     return (
         <div className='flex flex-col gap-5 m-3 md:m-10'>
-            <h1 className="mb-2 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">{beer.name}</h1>
-
             <div className='flex flex-col md:flex-row justify-evenly gap-5'>
-                <Image
-                    isZoomed={true}
-                    as={NextImage}
-                    width={500}
-                    height={500}
-                    quality={100}
-                    alt="NextUI Fruit Image with Zoom"
-                    src={beer.evidence_img || ""}
-                />
 
-                <div>
-                    <p>ML: {beer.ml} </p>
-                    <p>%: {beer.alcohol_percentage} </p>
-                    <p>Impression: {beer.initial_impression} </p>
+                <div className='flex flex-col gap-2'>
+                    <h1 className="mb-5 lg:ml-[-40px] text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">{beer.name}</h1>
 
-
-                    <FlagAvatar value={beer.country} withName={true}/>
+                    <Image
+                        isZoomed={true}
+                        as={NextImage}
+                        width={500}
+                        height={500}
+                        quality={100}
+                        alt={`${beer.name} ${t('beer')} ${t('image')}`}
+                        src={beer.evidence_img || ""}
+                    />
+                </div>
 
 
-                    <p>Bought In: {beer.bought_in} </p>
+                <div className='flex flex-col gap-2'>
 
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">{t('ml')} </p>
+                        <span className={'font-normal'}>{beer.ml}</span>
+                    </div>
+
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">{t('alcohol')}</p>
+                        <span className={'font-normal'}>{beer.alcohol_percentage} {t('abv')}</span>
+                    </div>
+
+
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">{t('impression')}</p>
+                        <span className={'font-normal'}>{beer.initial_impression} </span>
+                    </div>
+
+
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">{t('country')}</p>
+                        <FlagAvatar value={beer.country} withName={true}/>
+                    </div>
+
+
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">{t('impression')}</p>
+                        <span className={'font-normal'}>{beer.initial_impression} </span>
+                    </div>
+
+                    <div className='flex gap-2 items-center'>
+                        <p className="text-2xl font-bold">Bought In:
+                        </p>
+
+                        <span className={'font-normal'}>{beer.bought_in}</span>
+                    </div>
                 </div>
 
             </div>
-
         </div>)
 }
 
--- modules/UI/UiComponent.tsx
+++ modules/UI/UiComponent.tsx
@@ -1,59 +1,71 @@
-'use client'
+"use client";
 
-import React, {useEffect, useState} from "react";
-import {useBeerStore} from "@/store/zustand";
-import {BeerType, CARD_MODE, DARK_MODE, TABLE_MODE} from "@/util/types";
+import React, { useEffect, useState } from "react";
+import { useBeerStore } from "@/store/zustand";
+import { BeerType, CARD_MODE, DARK_MODE, TABLE_MODE } from "@/util/types";
 import MobileSearchControls from "./CardsUI/SearchControls/MobileSearchControls";
 import BeerGrid from "@/modules/UI/CardsUI/BeerGrid/BeerGrid";
-import {Skeleton} from "@nextui-org/react";
+import { Skeleton } from "@nextui-org/react";
 import BeersTable from "@/modules/UI/TableUI/DataTable/BeersTable";
 
 interface UiComponentProps {
-    serverFetchedBeers: Array<BeerType>
+  serverFetchedBeers: Array<BeerType>;
 }
 
-function UiComponent({serverFetchedBeers}: UiComponentProps) {
+function UiComponent({ serverFetchedBeers }: UiComponentProps) {
+  const [loadingData, setLoadingData] = useState<boolean>(true);
+  const { theme, mode, setMode, allBeers, setAllBeers } = useBeerStore();
 
-    const [loadingData, setLoadingData] = useState<boolean>(true)
-    const {theme, mode, setMode, allBeers, setAllBeers} = useBeerStore();
+  useEffect(() => {
+    const getMode = window.innerWidth < 1024 ? CARD_MODE : TABLE_MODE;
+    setMode(getMode);
+    setAllBeers(serverFetchedBeers);
+    setLoadingData(false);
+  }, [setMode, serverFetchedBeers, setAllBeers]);
 
-
-    useEffect(() => {
-        const getMode = window.innerWidth < 1024 ? CARD_MODE : TABLE_MODE
-        setMode(getMode)
-        setAllBeers(serverFetchedBeers)
-        setLoadingData(false)
-    }, [setMode, serverFetchedBeers, setAllBeers])
-
-
-    if (loadingData && allBeers.length === 0 && !mode) {
-        return (
-            <div className={`dataTableWrapper ${theme === DARK_MODE && 'dataTableWrapper__dark'}`}>
-                <Skeleton className="rounded-lg">
-                    <div className="h-[600px] w-full rounded-lg bg-default-300"></div>
-                </Skeleton>
-            </div>
-        )
-    }
-
+  if (loadingData && allBeers.length === 0 && !mode) {
     return (
+      <div
+        className={`dataTableWrapper ${
+          theme === DARK_MODE && "dataTableWrapper__dark"
+        }`}
+      >
+        <Skeleton className="rounded-lg">
+          <div className="h-[600px] w-full rounded-lg bg-default-300"></div>
+        </Skeleton>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {mode === CARD_MODE && (
         <>
-            {mode === CARD_MODE && <>
-                <div className={`dataCardWrapper ${theme === DARK_MODE ? 'dataCardWrapper__dark' : ""}`}>
-                    <BeerGrid/>
-                </div>
+          <div
+            className={`dataCardWrapper ${
+              theme === DARK_MODE ? "dataCardWrapper__dark" : ""
+            }`}
+          >
+            <BeerGrid />
+          </div>
 
-                <MobileSearchControls/>
-            </>}
+          <MobileSearchControls />
+        </>
+      )}
 
-            {mode === TABLE_MODE && <>
-                <div className={`dataTableWrapper ${theme === DARK_MODE ? 'dataTableWrapper__dark' : ""}`}>
-                    <BeersTable/>
-                </div>
-            </>}
-        </>)
-
+      {mode === TABLE_MODE && (
+        <>
+          <div
+            className={`dataTableWrapper ${
+              theme === DARK_MODE ? "dataTableWrapper__dark" : ""
+            }`}
+          >
+            <BeersTable />
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
-
-export default UiComponent
+export default UiComponent;
