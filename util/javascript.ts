9750778506ad5a8daca166fe1d7a18a795365193
Kbueno1077 <kbueno1077@gmail.com>
2024-03-05 23:26:57 -0500

    Fix Js error

--- util/javascript.ts
+++ util/javascript.ts
@@ -1,27 +1,35 @@
-import {getSession} from "next-auth/react";
+import { getSession } from "next-auth/react";
 
-export const countryFlagsToTheLeft = ['cu', 'pr']
+export const countryFlagsToTheLeft = ["cu", "pr"];
 
 export function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export async function checkSession() {
-    const session = await getSession()
+    const session = await getSession();
 
-    if (session) return session
-    throw ({error: true, response: {data: {errorMessage: "Log in to do this operation"}}})
+    if (session) return session;
+    throw {
+        error: true,
+        response: { data: { errorMessage: "Log in to do this operation" } },
+    };
 }
-
 
 export function groupByMethod(array: any, property: any = "") {
     let result = array.reduce((result: any, item: any) => {
-        let key = 'all';
+        let key = "all";
         if (item[property]) {
-            if (property === 'name') {
-                key = item[property][0].match(/[0-9]/) ? '#' : item[property][0];
-            } else if (property === 'country' && item[property] === 'TBD' && item[property] === "") {
-                key = 'zzzTBD'; // This will ensure 'TBD' sorts last
+            if (property === "name") {
+                key = item[property][0].match(/[0-9]/)
+                    ? "#"
+                    : item[property][0];
+            } else if (
+                property === "country" &&
+                item[property] === "TBD" &&
+                item[property] === ""
+            ) {
+                key = "zzzTBD"; // This will ensure 'TBD' sorts last
             } else {
                 key = item[property];
             }
@@ -33,27 +41,29 @@ export function groupByMethod(array: any, property: any = "") {
     result.total = array.length;
 
     // If 'TBD' group exists, rename 'zzzTBD' to 'TBD'
-    if (result['zzzTBD']) {
-        result['TBD'] = result['zzzTBD'];
-        delete result['zzzTBD'];
+    if (result["zzzTBD"]) {
+        result["TBD"] = result["zzzTBD"];
+        delete result["zzzTBD"];
     }
 
     // Assign order of appearance number to each item
     let order = 1;
+    let resultWithOrder = { ...result };
+
     for (let key in result) {
-        if (key !== 'total') {
-            result[key].forEach((item: any) => {
-                item.order = order++;
+        if (key !== "total") {
+            resultWithOrder[key] = result[key].map((item: any) => {
+                let newItem = { ...item, order: order++ };
+                return newItem;
             });
         }
     }
 
-    return result;
+    return resultWithOrder;
 }
 
-
 export const sortBy = (array: any, sortProp: string, direction: string) => {
-    if (direction === 'up') {
+    if (direction === "up") {
         return array.sort((a: any, b: any) => {
             if (a[sortProp] < b[sortProp]) {
                 return -1;
@@ -62,7 +72,7 @@ export const sortBy = (array: any, sortProp: string, direction: string) => {
                 return 1;
             }
             return 0;
-        })
+        });
     } else {
         return array.sort((a: any, b: any) => {
             if (a[sortProp] < b[sortProp]) {
@@ -72,7 +82,6 @@ export const sortBy = (array: any, sortProp: string, direction: string) => {
                 return -1;
             }
             return 0;
-        })
-
+        });
     }
-}
+};
