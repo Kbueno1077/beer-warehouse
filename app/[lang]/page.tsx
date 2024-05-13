91bfb69fd1aaf190f9a0a0d8c612a745ec8c20b3
Kbueno1077 <kbueno1077@gmail.com>
2024-05-12 22:18:23 -0400

    go Back Page cookies test

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -5,23 +5,13 @@ import { getXataClient } from "@/xata/xata";
 import UiComponent from "@/modules/UI/UiComponent";
 import Image from "next/image";
 import { getServerSession } from "next-auth";
-import { cookies } from "next/headers";
-import createCookie from "../actions";
 
 const Page: NextPage = async () => {
+    const session = await getServerSession();
     const xata = getXataClient();
-    const cookieStore = cookies();
-    const hasCookie = cookieStore.has("warehouseOwner");
-    const warehouseOwner = cookieStore?.get("warehouseOwner")?.value;
-
-    async function create(name: string, value: string) {
-        "use server";
-
-        cookies().set(name, value);
-    }
 
     let serverFetchedBeers = [];
-    if (!hasCookie || warehouseOwner === "Kevin") {
+    if (!session || !session.user || session?.user.name === "Kevin") {
         serverFetchedBeers = await xata.db.beers
             .select([
                 "id",
@@ -36,8 +26,6 @@ const Page: NextPage = async () => {
             ])
             .sort("name", "asc")
             .getAll();
-
-        create("warehouseOwner", "Kevin");
     } else {
         serverFetchedBeers = await xata.db.usersBeers
             .select([
@@ -52,7 +40,7 @@ const Page: NextPage = async () => {
                 "additional_comments",
                 "owner",
             ])
-            .filter({ owner: warehouseOwner })
+            .filter({ owner: session.user.name })
             .sort("name", "asc")
             .getAll();
     }
@@ -80,3 +68,86 @@ const Page: NextPage = async () => {
 };
 
 export default Page;
+
+// import React from "react";
+// import { NextPage } from "next";
+
+// import { getXataClient } from "@/xata/xata";
+// import UiComponent from "@/modules/UI/UiComponent";
+// import Image from "next/image";
+// import { getServerSession } from "next-auth";
+// import { cookies } from "next/headers";
+// import createCookie from "../actions";
+
+// const Page: NextPage = async () => {
+//     const xata = getXataClient();
+//     const cookieStore = cookies();
+//     const hasCookie = cookieStore.has("warehouseOwner");
+//     const warehouseOwner = cookieStore?.get("warehouseOwner")?.value;
+
+//     async function create(name: string, value: string) {
+//         "use server";
+
+//         cookies().set(name, value);
+//     }
+
+//     let serverFetchedBeers = [];
+//     if (!hasCookie || warehouseOwner === "Kevin") {
+//         serverFetchedBeers = await xata.db.beers
+//             .select([
+//                 "id",
+//                 "name",
+//                 "alcohol_percentage",
+//                 "ml",
+//                 "country",
+//                 "initial_impression",
+//                 "bought_in",
+//                 "evidence_img",
+//                 "additional_comments",
+//             ])
+//             .sort("name", "asc")
+//             .getAll();
+
+//         create("warehouseOwner", "Kevin");
+//     } else {
+//         serverFetchedBeers = await xata.db.usersBeers
+//             .select([
+//                 "id",
+//                 "name",
+//                 "alcohol_percentage",
+//                 "ml",
+//                 "country",
+//                 "initial_impression",
+//                 "bought_in",
+//                 "evidence_img",
+//                 "additional_comments",
+//                 "owner",
+//             ])
+//             .filter({ owner: warehouseOwner })
+//             .sort("name", "asc")
+//             .getAll();
+//     }
+
+//     return (
+//         <>
+//             <div>
+//                 <div className="bgWallpaper">
+//                     <Image
+//                         src="/beerglasses.png"
+//                         alt={"beerImages"}
+//                         priority={true}
+//                         quality={100}
+//                         width={1150}
+//                         height={500}
+//                     />
+//                 </div>
+
+//                 <UiComponent
+//                     serverFetchedBeers={JSON.stringify(serverFetchedBeers)}
+//                 />
+//             </div>
+//         </>
+//     );
+// };
+
+// export default Page;
